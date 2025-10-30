e3f915f49b6f2ded421844455042422dd40f644e
Diana Tikhonova <dixi.tix@gmail.com>
2025-10-30 12:14:07 +0300

    feat: connect main service to postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 	"social-network/internal/handlers"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = "postgres://main_user:main_pass@localhost:5433/main_db?sslmode=disable"
+	}
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		panic(fmt.Errorf("db ping failed: %w", err))
+	}
+	fmt.Println("Connected to Postgres")
+
 	http.HandleFunc("/health", handlers.Health)
 
 	fmt.Println("Main server started on :8080")
